897865dec1f91a55b16f4bd27d892f48e77abcce
nyxrotten <nes_calina@hotmail.com>
2024-05-20 16:49:21 +0200

    feat: updateProduct

--- product.js
+++ product.js
@@ -1,35 +1,64 @@
 const products = [];
 let id = 0;
 
+
 function resetProducts(){};
 
 function addProduct(name, price){
-    if(name === undefined) {
-        throw new Error ("name is not defined")
+    if (typeof name !== "string") {
+        throw new Error ("name is not defined or not string")
     }
     else if (price === undefined) {
         throw new Error ("price is not defined")
     }
-    else if (products.find((product) => product === name)){
-        throw new Error ("Product already on the list")
-    }
+    // else if (name === products.forEach((item) => Object.values(item))) {
+    //     console.log(error)
+    // }
     else {
         id = ++id;
         products.push({productId: id, productName: name, productPrice: price});
-    
     }
 };
 
 function removeProduct(id){};
 
-function getProducts(){};
+function getProducts(id) {
+    let ids = [];
+    products.forEach((item) => ids.push(item.id));
+    let find = ids.find((element) => element === id);
+    
+    if (id === find) {
+        return products[`${--find}`]
+    }
+    else {
+        throw new Error ("Product not found")
+    }
+};
+
 
 function resetProducts(){};
 
-function getProduct(id){};
 
-function updateProduct(id, name, price){};
 
-addProduct("lemon", 20);
-addProduct("orange", 20);
+function updateProduct(id, name, price) {
+    const ids = [];
+    products.forEach((product) => ids.push(product.productId));
+    console.log(ids)
+    foundId = ids.find((element) => element === id);
+    
+    if (!foundId) {
+        return "Item doesn't exist"
+    }
+    else { 
+        const index = ids.indexOf(id);
+        products[`${index}`].productName = name;
+        products[`${index}`].productPrice = price;
+    }
+}
+
+
+
+addProduct("lemon", 2)
+
+
 console.log(products)
