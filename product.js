a931cb62ea73dcdfb7285ef483e7ccd66d0a45b7
nyxrotten <nes_calina@hotmail.com>
2024-05-21 10:57:25 +0200

    commit

--- product.js
+++ product.js
@@ -1,8 +1,11 @@
-const products = [];
+let products = [];
 let id = 0;
 
 
-function resetProducts(){};
+const resetProducts = () => {
+    id = 0;
+    products = [];
+};
 
 function addProduct(name, price){
     if (typeof name !== "string") {
@@ -11,54 +14,73 @@ function addProduct(name, price){
     else if (price === undefined) {
         throw new Error ("price is not defined")
     }
-    // else if (name === products.forEach((item) => Object.values(item))) {
-    //     console.log(error)
-    // }
     else {
         id = ++id;
         products.push({productId: id, productName: name, productPrice: price});
     }
 };
 
-function removeProduct(id){};
 
-function getProducts(id) {
-    let ids = [];
-    products.forEach((item) => ids.push(item.id));
-    let find = ids.find((element) => element === id);
-    
-    if (id === find) {
-        return products[`${--find}`]
+function getProduct(id) {
+    const product = products.find((item) => item.productId === id);
+    if (product !== undefined) {
+        return product
     }
     else {
-        throw new Error ("Product not found")
+        throw new Error ("Product doesn't exist");
     }
 };
-
-
-function resetProducts(){};
 
 
 
 function updateProduct(id, name, price) {
     const ids = [];
     products.forEach((product) => ids.push(product.productId));
-    console.log(ids)
     foundId = ids.find((element) => element === id);
-    
+
     if (!foundId) {
         return "Item doesn't exist"
     }
-    else { 
+    else {
         const index = ids.indexOf(id);
         products[`${index}`].productName = name;
         products[`${index}`].productPrice = price;
     }
 }
 
+const getProducts = () => {
+    return products
+    };
 
+const removeProduct = (id) => {
+        let product = products.find((item) => item.productId === id);
+        if (product !== undefined) {
+            delete product.productId;
+            delete product.productName;
+            delete product.productPrice;
+        }
+        else if (id === undefined) {
+            return
+        }
+        else {
+            throw new Error ("Product doesn't exist");
+        }
+    };
+    
 
-addProduct("lemon", 2)
+addProduct("lemon", 2) //añadir primer producto
+updateProduct(1, "orange", 3) //editar el producto
+getProduct(1) //obtener el producto
+getProducts()
 
-
+//resetProducts()
+removeProduct(1)
 console.log(products)
+
+module.exports = {
+    updateProduct,
+    getProduct,
+    resetProducts,
+    getProducts,
+    removeProduct,
+    addProduct};
--- product.test.js
+++ product.test.js
@@ -1,1 +1,46 @@
-const { resetProducts,  addProduct, removeProduct, getProducts, getProduct, updateProduct} = require('./product');
+const { updateProduct, getProduct, resetProducts, getProducts, removeProduct, addProduct} = require('./product');
+
+describe("updateProduct", () => {
+    it ("should update an existing product", () => {
+        expect(() => updateProduct(1, "orange", 3)).not.toThrow()
+    });
+    it ("shouldn't find product", () => {
+        expect(() => updateProduct(3, "orange", 3)).not.toBe(true)
+    });
+});
+
+describe("getProduct", () => {
+    it ("should get product by id", () => {
+        expect(() => getProduct(1).not.toThrow())
+    });
+    it ("shouldn't find product", () => {
+        expect(() => getProduct(2).not.toBe(true))
+    })
+});
+
+describe("resetProducts", () => {
+    it ("should erase products and ids", () => {
+        expect(() => getProduct(2).toBeUndefined())
+    });
+});
+
+describe("getProducts", () => {
+    it("should return products", () => {
+        expect(() => getProducts.not.toThrow())
+    })
+});
+
+describe("getProducts", () => {
+    it("should return products", () => {
+        expect(() => getProducts.not.toThrow())
+    })
+});
+
+describe("addProduct", () => {
+    it("should add product", () => {
+        expect(() => addProduct("lemon", 2).not.ToThrow())
+    });
+    it("should throw error if name or price isn't defined", () => {
+        expect(() => addProduct("lemon").toThrow())
+    })
+})
